d547e65e1063c0149ea7a8014da7154fb1928f51
harishlikespi <73505107+harishlikespi@users.noreply.github.com>
2021-02-13 18:28:17 -0800

    Update MS5611.cpp

--- embedded/MS5611.cpp
+++ embedded/MS5611.cpp
@@ -1,263 +1,249 @@
-/*
-MS5611.cpp - Class file for the MS5611 Barometric Pressure & Temperature Sensor Arduino Library.
+//
+//    FILE: MS5611.cpp
+//  AUTHOR: Rob Tillaart
+//          Erni - testing/fixes
+// VERSION: 0.3.0
+// PURPOSE: MS5611 Temperature & Humidity library for Arduino
+//     URL: https://github.com/RobTillaart/MS5611
+//
+//  HISTORY:
+//  0.3.0   2021-01-27  fix #9 math error (thanks to Emiel Steerneman)
+//                      add Wire1..WireN support (e.g. teensy)
+//                      changed getTemperature() and getPressure()
+//                      add reset()
+//  0.2.2   2021-01-01  add Arduino-CI + unit tests + isConnected()
+//  0.2.1   2020-06-28  fix #1 min macro compile error
+//  0.2.0   2020-06-21  refactor; #pragma once; 
+//  0.1.8               fix #109 incorrect constants (thanks to flauth)
+//  0.1.7               revert double to float (issue 33)
+//  0.1.6   2015-07-12  refactor
+//  0.1.05  moved 6 float multiplies to init  [adds ~70 bytes !!!]
+//          moved the MS5611_LIB_VERSION to PROGMEM
+//  0.1.04  changed float to double (for platforms which support it)
+//          changed divisions in multiplications
+//          fixed uint32_t readADC()
+//          reduced size of C array by 1 float
+//          added second order temperature compensation
+//  0.1.03  changed math to float [test version]
+//  0.1.02  fixed bug return value read()
+//          fixed bug #bits D2
+//          added MS5611_READ_OK
+//          added inline getters for temp & pres & lastresult.
+//          adjusted delay's based on datasheet
+//          merged convert functions
+//          fixed offset in readProm()
+//  0.1.01  small refactoring
+//  0.1.00  added temperature and Pressure code
+//  0.0.00  initial version by Rob Tillaart (15-okt-2014)
 
-Version: 1.0.0
-(c) 2014 Korneliusz Jarzebski
-www.jarzebski.pl
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the version 3 GNU General Public License as
-published by the Free Software Foundation.
+#include "MS5611.h"
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+// datasheet page 10
+#define MS5611_CMD_READ_ADC       0x00
+#define MS5611_CMD_READ_PROM      0xA0
+#define MS5611_CMD_RESET          0x1E
+#define MS5611_CMD_CONVERT_D1     0x40
+#define MS5611_CMD_CONVERT_D2     0x50
 
-#if ARDUINO >= 100
-#include "Arduino.h"
-#else
-#include "WProgram.h"
+
+/////////////////////////////////////////////////////
+//
+// PUBLIC
+//
+MS5611::MS5611(uint8_t deviceAddress)
+{
+  _address     = deviceAddress;
+  _temperature = MS5611_NOT_READ;
+  _pressure    = MS5611_NOT_READ;
+  _result      = MS5611_NOT_READ;
+  _lastRead    = 0;
+}
+
+
+#if defined (ESP8266) || defined(ESP32)
+bool MS5611::begin(uint8_t dataPin, uint8_t clockPin, TwoWire * wire)
+{
+  if ((_address < 0x76) || (_address > 0x77)) return false;
+
+  _wire = wire;
+  if ((dataPin < 255) && (clockPin < 255))
+  {
+    _wire->begin(dataPin, clockPin);
+  } else {
+    _wire->begin();
+  }
+  if (! isConnected()) return false;
+
+  reset();
+  return true;
+}
 #endif
 
-#include <Wire.h>
-#include <math.h>
 
-#include <MS5611.h>
-
-bool MS5611::begin(ms5611_osr_t osr)
+bool MS5611::begin(TwoWire * wire)
 {
-    Wire.begin();
+  if ((_address < 0x76) || (_address > 0x77)) return false;
+  _wire = wire;
+  _wire->begin();
+  if (! isConnected()) return false;
 
-    reset();
-
-    setOversampling(osr);
-
-    delay(100);
-
-    readPROM();
-
-    return true;
+  reset();
+  return true;
 }
 
-// Set oversampling value
-void MS5611::setOversampling(ms5611_osr_t osr)
+
+bool MS5611::isConnected()
 {
-    switch (osr)
+  _wire->beginTransmission(_address);
+  return (_wire->endTransmission() == 0);
+}
+
+
+void MS5611::reset()
+{
+  command(MS5611_CMD_RESET);
+  delay(3);
+  // constants that were multiplied in read()
+  // do this once and you save CPU cycles
+  C[0] = 1;
+  C[1] = 32768L;
+  C[2] = 65536L;
+  C[3] = 3.90625E-3;
+  C[4] = 7.8125E-3;
+  C[5] = 256;
+  C[6] = 1.1920928955E-7;
+  // read factory calibrations from EEPROM.
+  for (uint8_t reg = 0; reg < 7; reg++)
+  {
+    // used indices match datasheet.
+    // C[0] == manufacturer - read but not used;
+    // C[7] == CRC - skipped.
+    C[reg] *= readProm(reg);
+  }
+}
+
+
+int MS5611::read(uint8_t bits)
+{
+  // VARIABLES NAMES BASED ON DATASHEET
+  // ALL MAGIC NUMBERS ARE FROM DATASHEET
+
+  convert(MS5611_CMD_CONVERT_D1, bits);
+  if (_result) return _result;
+  uint32_t D1 = readADC();
+  if (_result) return _result;
+
+  convert(MS5611_CMD_CONVERT_D2, bits);
+  if (_result) return _result;
+  uint32_t D2 = readADC();
+  if (_result) return _result;
+  
+  //  TEST VALUES - comment lines above
+  // uint32_t D1 = 9085466;
+  // uint32_t D2 = 8569150;
+
+  // TEMP & PRESS MATH - PAGE 7/20
+  float dT = D2 - C[5];
+  _temperature = 2000 + dT * C[6];
+
+  float offset =  C[2] + dT * C[4];
+  float sens = C[1] + dT * C[3];
+
+  // SECOND ORDER COMPENSATION - PAGE 8/20
+  // COMMENT OUT < 2000 CORRECTION IF NOT NEEDED
+  // NOTE TEMPERATURE IS IN 0.01 C
+  if (_temperature < 2000)
+  {
+    float T2 = dT * dT * 4.6566128731E-10;
+    float t = (_temperature - 2000) * (_temperature - 2000);
+    float offset2 = 2.5 * t;
+    float sens2 = 1.25 * t;
+    // COMMENT OUT < -1500 CORRECTION IF NOT NEEDED
+    if (_temperature < -1500)
     {
-	case MS5611_ULTRA_LOW_POWER:
-	    ct = 1;
-	    break;
-	case MS5611_LOW_POWER:
-	    ct = 2;
-	    break;
-	case MS5611_STANDARD:
-	    ct = 3;
-	    break;
-	case MS5611_HIGH_RES:
-	    ct = 5;
-	    break;
-	case MS5611_ULTRA_HIGH_RES:
-	    ct = 10;
-	    break;
+      t = (_temperature + 1500) * (_temperature + 1500);
+      offset2 += 7 * t;
+      sens2 += 5.5 * t;
     }
+    _temperature -= T2;
+    offset -= offset2;
+    sens -= sens2;
+  }
+  // END SECOND ORDER COMPENSATION
 
-    uosr = osr;
+  _pressure = (D1 * sens * 4.76837158205E-7 - offset) * 3.051757813E-5;
+
+  _lastRead = millis();
+  return MS5611_READ_OK;
 }
 
-// Get oversampling value
-ms5611_osr_t MS5611::getOversampling(void)
+
+/////////////////////////////////////////////////////
+//
+// PRIVATE
+//
+void MS5611::convert(const uint8_t addr, uint8_t bits)
 {
-    return (ms5611_osr_t)uosr;
+  uint8_t del[5] = {1, 2, 3, 5, 10};
+
+  bits = constrain(bits, 8, 12);
+  uint8_t offset = (bits - 8) * 2;
+  command(addr + offset);
+  delay(del[offset/2]);
 }
 
-void MS5611::reset(void)
+
+uint16_t MS5611::readProm(uint8_t reg)
 {
-    Wire.beginTransmission(MS5611_ADDRESS);
+  // last EEPROM register is CRC - Page13 datasheet.
+  uint8_t promCRCRegister = 7;
+  if (reg > promCRCRegister) return 0;
 
-    #if ARDUINO >= 100
-	Wire.write(MS5611_CMD_RESET);
-    #else
-	Wire.send(MS5611_CMD_RESET);
-    #endif
-
-    Wire.endTransmission();
-}
-
-void MS5611::readPROM(void)
-{
-    for (uint8_t offset = 0; offset < 6; offset++)
+  uint8_t offset = reg * 2;
+  command(MS5611_CMD_READ_PROM + offset);
+  if (_result == 0)
+  {
+    int nr = _wire->requestFrom(_address, (uint8_t)2);
+    if (nr >= 2)
     {
-	fc[offset] = readRegister16(MS5611_CMD_READ_PROM + (offset * 2));
+      uint16_t val = _wire->read() * 256;
+      val += _wire->read();
+      return val;
     }
+    return 0;
+  }
+  return 0;
 }
 
-uint32_t MS5611::readRawTemperature(void)
+
+uint32_t MS5611::readADC()
 {
-    Wire.beginTransmission(MS5611_ADDRESS);
-
-    #if ARDUINO >= 100
-	Wire.write(MS5611_CMD_CONV_D2 + uosr);
-    #else
-	Wire.send(MS5611_CMD_CONV_D2 + uosr);
-    #endif
-
-    Wire.endTransmission();
-
-    delay(ct);
-
-    return readRegister24(MS5611_CMD_ADC_READ);
-}
-
-uint32_t MS5611::readRawPressure(void)
-{
-    Wire.beginTransmission(MS5611_ADDRESS);
-
-    #if ARDUINO >= 100
-	Wire.write(MS5611_CMD_CONV_D1 + uosr);
-    #else
-	Wire.send(MS5611_CMD_CONV_D1 + uosr);
-    #endif
-
-    Wire.endTransmission();
-
-    delay(ct);
-
-    return readRegister24(MS5611_CMD_ADC_READ);
-}
-
-int32_t MS5611::readPressure(bool compensation)
-{
-    uint32_t D1 = readRawPressure();
-
-    uint32_t D2 = readRawTemperature();
-    int32_t dT = D2 - (uint32_t)fc[4] * 256;
-
-    int64_t OFF = (int64_t)fc[1] * 65536 + (int64_t)fc[3] * dT / 128;
-    int64_t SENS = (int64_t)fc[0] * 32768 + (int64_t)fc[2] * dT / 256;
-
-    if (compensation)
+  command(MS5611_CMD_READ_ADC);
+  if (_result == 0)
+  {
+    int nr = _wire->requestFrom(_address, (uint8_t)3);
+    if (nr >= 3)
     {
-	int32_t TEMP = 2000 + ((int64_t) dT * fc[5]) / 8388608;
-
-	OFF2 = 0;
-	SENS2 = 0;
-
-	if (TEMP < 2000)
-	{
-	    OFF2 = 5 * ((TEMP - 2000) * (TEMP - 2000)) / 2;
-	    SENS2 = 5 * ((TEMP - 2000) * (TEMP - 2000)) / 4;
-	}
-
-	if (TEMP < -1500)
-	{
-	    OFF2 = OFF2 + 7 * ((TEMP + 1500) * (TEMP + 1500));
-	    SENS2 = SENS2 + 11 * ((TEMP + 1500) * (TEMP + 1500)) / 2;
-	}
-
-	OFF = OFF - OFF2;
-	SENS = SENS - SENS2;
+      uint32_t val = _wire->read() * 65536UL;
+      val += _wire->read() * 256UL;
+      val += _wire->read();
+      return val;
     }
-
-    uint32_t P = (D1 * SENS / 2097152 - OFF) / 32768;
-
-    return P;
+    return 0UL;
+  }
+  return 0UL;
 }
 
-double MS5611::readTemperature(bool compensation)
+
+int MS5611::command(const uint8_t command)
 {
-    uint32_t D2 = readRawTemperature();
-    int32_t dT = D2 - (uint32_t)fc[4] * 256;
-
-    int32_t TEMP = 2000 + ((int64_t) dT * fc[5]) / 8388608;
-
-    TEMP2 = 0;
-
-    if (compensation)
-    {
-	if (TEMP < 2000)
-	{
-	    TEMP2 = (dT * dT) / (2 << 30);
-	}
-    }
-
-    TEMP = TEMP - TEMP2;
-
-    return ((double)TEMP/100);
+  yield();
+  _wire->beginTransmission(_address);
+  _wire->write(command);
+  _result = _wire->endTransmission();
+  return _result;
 }
 
-// Calculate altitude from Pressure & Sea level pressure
-double MS5611::getAltitude(double pressure, double seaLevelPressure)
-{
-    return (44330.0f * (1.0f - pow((double)pressure / (double)seaLevelPressure, 0.1902949f)));
-}
-
-// Calculate sea level from Pressure given on specific altitude
-double MS5611::getSeaLevel(double pressure, double altitude)
-{
-    return ((double)pressure / pow(1.0f - ((double)altitude / 44330.0f), 5.255f));
-}
-
-// Read 16-bit from register (oops MSB, LSB)
-uint16_t MS5611::readRegister16(uint8_t reg)
-{
-    uint16_t value;
-    Wire.beginTransmission(MS5611_ADDRESS);
-    #if ARDUINO >= 100
-        Wire.write(reg);
-    #else
-        Wire.send(reg);
-    #endif
-    Wire.endTransmission();
-
-    Wire.beginTransmission(MS5611_ADDRESS);
-    Wire.requestFrom(MS5611_ADDRESS, 2);
-    while(!Wire.available()) {};
-    #if ARDUINO >= 100
-        uint8_t vha = Wire.read();
-        uint8_t vla = Wire.read();
-    #else
-        uint8_t vha = Wire.receive();
-        uint8_t vla = Wire.receive();
-    #endif;
-    Wire.endTransmission();
-
-    value = vha << 8 | vla;
-
-    return value;
-}
-
-// Read 24-bit from register (oops XSB, MSB, LSB)
-uint32_t MS5611::readRegister24(uint8_t reg)
-{
-    uint32_t value;
-    Wire.beginTransmission(MS5611_ADDRESS);
-    #if ARDUINO >= 100
-        Wire.write(reg);
-    #else
-        Wire.send(reg);
-    #endif
-    Wire.endTransmission();
-
-    Wire.beginTransmission(MS5611_ADDRESS);
-    Wire.requestFrom(MS5611_ADDRESS, 3);
-    while(!Wire.available()) {};
-    #if ARDUINO >= 100
-        uint8_t vxa = Wire.read();
-        uint8_t vha = Wire.read();
-        uint8_t vla = Wire.read();
-    #else
-        uint8_t vxa = Wire.receive();
-        uint8_t vha = Wire.receive();
-        uint8_t vla = Wire.receive();
-    #endif;
-    Wire.endTransmission();
-
-    value = ((int32_t)vxa << 16) | ((int32_t)vha << 8) | vla;
-
-    return value;
-}
+// -- END OF FILE --
